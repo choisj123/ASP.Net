7dc3148ce88b2cf180d6bd08b45ae1f087b121f1
Sujin Choi(최수진 - Q-Service Dev. Team) <sujinchoi@qoo10.com>
2023-08-21 17:52:54 +0900

    Question 컬렉션으로 리스트 생성

---
+++ CShapPractice/CShapPractice/TestCode.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CShapPractice
+{
+    public partial class TestCode : System.Web.UI.Page
+    {
+        // 열거형(enum)
+        enum UserType
+        {
+            Admin, 
+            Master,
+            Guest = 5,
+            Invalid
+        }
+
+        // 구조체(struct)
+        public struct Dog
+        {
+            public string Name;
+            public string Breed;
+
+            public Dog(string name, string breed)
+            {
+                Name = name;
+                Breed = breed;
+            }
+
+            public string Introduce()
+            {
+                return "이름: " + Name + "(" + Breed + ")";
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UserType siteUserType = UserType.Guest;
+            ShowResult(siteUserType + " = " + (int)siteUserType);
+
+            Dog bob = new Dog("몽실이", "말티즈");
+            Dog tom;
+            tom.Name = "유하";
+            tom.Breed = "폼피츠";
+
+            ShowResult(bob.Introduce());
+            ShowResult(tom.Introduce());
+        }
+
+        void ShowResult(string result)
+        {
+            Label lblResult = new Label();
+            Page.Controls.Add(lblResult);
+            lblResult.Text = result + "<br/>";
+        }
+    }
+}
--- QuestionAnswer/QuestionAnswer/Program.cs
+++ QuestionAnswer/QuestionAnswer/Program.cs
@@ -10,306 +10,146 @@ namespace QuestionAnswer
     {
         class Question
         {
-            string QuestionText;
-            string[] Options;
-            int CorrectAnswer;
+            public string QuestionText { get; set; }
+            public string[] Options { get; set; }
+            public int CorrectAnswer { get; set; }
 
         }
 
         static void Main(string[] args)
         {
-
-            // 정답 카운트
-            int count = 0;
-
-
-            Console.WriteLine("출제를 시작합니다!");
-            Console.WriteLine("====================================");
-            Console.WriteLine("질문 1. 우리나라 최초의 국가는?");
-            Console.WriteLine("1) 고구려");
-            Console.WriteLine("2) 조선");
-            Console.WriteLine("3) 고조선");
-            Console.WriteLine("4) 고려");
-
-            Console.WriteLine("====================================");
-            Console.Write("정답을 입력하세요 >>>  ");
-
-            // 유효성 검증
-            string answer1 = Console.ReadLine();
-            int iAnswer1;
-            bool isNum = int.TryParse(answer1, out iAnswer1);
-           
-
-            if (isNum)
-             {
-                if(iAnswer1 == 3)
+            // 문제 목록
+            List<Question> questions = new List<Question> {
+                new Question
                 {
-                    Console.WriteLine("정답입니다!");
-                    count++;
-                }
-                else
+                    QuestionText = "우리나라 최초의 국가는?",
+                    Options = new string[] {"고구려", "조선", "고조선", "고려"},
+                    CorrectAnswer = 2
+                },
+
+                new Question
                 {
-                    Console.WriteLine("틀렸습니다.");
-                }
+                    QuestionText = "널리 인간을 이롭게 한다'는 고조선의 건국 이념은??",
+                    Options = new string[] {"고구려", "홍익인간", "고조선", "고려"},
+                    CorrectAnswer = 2
+                },
 
-             }
+                new Question
+                {
+                    QuestionText = "임진왜란이 일어난 연도는?",
+                    Options = new string[] {"1592년", "조선", "고조선", "고려"},
+                    CorrectAnswer = 1
+                },
 
-            Console.WriteLine("질문 2. '널리 인간을 이롭게 한다'는 고조선의 건국 이념은?");
-            Console.WriteLine("1) ");
-            Console.WriteLine("2) 홍익인간");
-            Console.WriteLine("3) ");
-            Console.WriteLine("4) ");
-            Console.WriteLine("====================================");
-            Console.Write("정답을 입력하세요 >>>  ");
+                new Question
+                {
+                    QuestionText = "조선의 수도는?",
+                    Options = new string[] {"고구려", "한양", "고조선", "고려"},
+                    CorrectAnswer = 2
+                },
 
-            // 유효성 검증
-            string answer2 = Console.ReadLine();
-            int iAnswer2;
-            bool isNum2 = int.TryParse(answer2, out iAnswer2);
+                new Question
+                {
+                    QuestionText = "우리나라는 언제 광복?",
+                    Options = new string[] {"고구려", "조선", "1945년 8월 15일", "고려"},
+                    CorrectAnswer = 3
+                },
 
+                new Question
+                {
+                    QuestionText = "6.25전쟁이 일어난 연도는?",
+                    Options = new string[] {"고구려", "조선", "고조선", "1950년"},
+                    CorrectAnswer = 4
+                },
 
-            if (isNum2)
+                new Question
+                {
+                    QuestionText = "이순신 장군이 학익진 전법으로 크게 승리한 해전의 이름은?",
+                    Options = new string[] {"한산도대첩", "조선", "고조선", "고려"},
+                    CorrectAnswer = 1
+                },
+
+                new Question
+                {
+                    QuestionText = "자유당의 부정과 3.15 부정 선거를 계기로 일어난 혁명의 이름은?",
+                    Options = new string[] {"고구려", "4.19혁명", "고조선", "고려"},
+                    CorrectAnswer = 2
+                },
+
+                new Question
+                {
+                    QuestionText = "임시 정부의 주석으로 한인 애국단을 조직하여 독립운동을 한 분은?",
+                    Options = new string[] {"고구려", "조선", "고조선", "김구"},
+                    CorrectAnswer = 4
+                },
+
+                new Question
+                {
+                    QuestionText = "민족대표 33인이 독립 선언서를 낭독하고 만세 삼창을 한 곳은?",
+                    Options = new string[] {"고구려", "탑골공원", "고조선", "고려"},
+                    CorrectAnswer = 2
+                },
+
+            };
+
+            Random random = new Random();
+            bool continueQuiz = true;
+
+            while (continueQuiz)
             {
-                if (iAnswer2 == 2)
+                // 정답 카운트
+                int count = 0;
+
+
+                Console.WriteLine("출제를 시작합니다!");
+
+                // 질문 리스트에서 꺼내기 
+                foreach(var question in questions)
                 {
-                    Console.WriteLine("정답입니다!");
-                    count++;
-                }
-                else
-                {
-                    Console.WriteLine("틀렸습니다.");
+                    Console.WriteLine("====================================");
+
+                    // 질문 텍스트 + 보기 꺼내기
+                    for(int i = 0; i < question.Options.Length; i++)
+                    {
+                        Console.WriteLine("질문 " + i + "." + question.QuestionText);
+                        Console.WriteLine($"{i + 1}) {question.Options[i]}");
+                    }
+
+                    Console.Write("정답을 입력하세요 >>> ");
+
+                    // 유효성 검증
+                    string answer = Console.ReadLine();
+                    int userAnswer;
+                    bool isNum = int.TryParse(answer, out userAnswer);
+
+                    if (isNum && userAnswer - 1 == question.CorrectAnswer)
+                    {
+                        Console.WriteLine("정답입니다!");
+                        count++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("틀렸습니다.");
+                    }
+                    // 10문제가 출제 끝
+
+                    float correctResult = ((float)count / questions.Count)*100;
+
+                    Console.WriteLine("총 " + questions.Count + "개의 문제 중에 " + count + "개의 문제를 맞추셨습니다. 정답률은 " + correctResult + "% 입니다.");
+
+                    Console.Write("퀴즈를 계속 푸시겠어요? (Y/N) >>> ");
+                    string continueInput = Console.ReadLine();
+
+
+                    }
+
                 }
 
             }
 
-            Console.WriteLine("질문 3. 임진왜란이 일어난 연도는? ");
-            Console.WriteLine("1) 1592년");
-            Console.WriteLine("2) ");
-            Console.WriteLine("3) ");
-            Console.WriteLine("4) ");
-            Console.WriteLine("====================================");
-            Console.Write("정답을 입력하세요 >>>  ");
-
-            // 유효성 검증
-            string answer3 = Console.ReadLine();
-            int iAnswer3;
-            bool isNum3 = int.TryParse(answer3, out iAnswer3);
 
 
-            if (isNum3)
-            {
-                if (iAnswer3 == 1)
-                {
-                    Console.WriteLine("정답입니다!");
-                    count++;
-                }
-                else
-                {
-                    Console.WriteLine("틀렸습니다.");
-                }
 
-            }
-
-            Console.WriteLine("질문 4. 조선의 수도는?");
-            Console.WriteLine("1) ");
-            Console.WriteLine("2) 한양 ");
-            Console.WriteLine("3) ");
-            Console.WriteLine("4) ");
-            Console.WriteLine("====================================");
-            Console.Write("정답을 입력하세요 >>>  ");
-
-            // 유효성 검증
-            string answer4 = Console.ReadLine();
-            int iAnswer4;
-            bool isNum4 = int.TryParse(answer4, out iAnswer4);
-
-
-            if (isNum4)
-            {
-                if (iAnswer4 == 2)
-                {
-                    Console.WriteLine("정답입니다!");
-                    count++;
-                }
-                else
-                {
-                    Console.WriteLine("틀렸습니다.");
-                }
-
-            }
-
-            Console.WriteLine("질문 5. 우리나라는 언제 광복?");
-            Console.WriteLine("1) ");
-            Console.WriteLine("2) 1945년 8월 15일");
-            Console.WriteLine("3) ");
-            Console.WriteLine("4) ");
-            Console.WriteLine("====================================");
-            Console.Write("정답을 입력하세요 >>>  ");
-
-            // 유효성 검증
-            string answer5 = Console.ReadLine();
-            int iAnswer5;
-            bool isNum5 = int.TryParse(answer5, out iAnswer5);
-
-
-            if (isNum5)
-            {
-                if (iAnswer5 == 2)
-                {
-                    Console.WriteLine("정답입니다!");
-                    count++;
-                }
-                else
-                {
-                    Console.WriteLine("틀렸습니다.");
-                }
-
-            }
-
-            Console.WriteLine("질문 6. 6.25전쟁이 일어난 연도는?");
-            Console.WriteLine("1) ");
-            Console.WriteLine("2) ");
-            Console.WriteLine("3) 1950년");
-            Console.WriteLine("4) ");
-            Console.WriteLine("====================================");
-            Console.Write("정답을 입력하세요 >>>  ");
-
-            // 유효성 검증
-            string answer6 = Console.ReadLine();
-            int iAnswer6;
-            bool isNum6 = int.TryParse(answer6, out iAnswer6);
-
-
-            if (isNum6)
-            {
-                if (iAnswer6 == 3)
-                {
-                    Console.WriteLine("정답입니다!");
-                    count++;
-                }
-                else
-                {
-                    Console.WriteLine("틀렸습니다.");
-                }
-
-            }
-
-            Console.WriteLine("질문 7. 이순신 장군이 학익진 전법으로 크게 승리한 해전의 이름은? ");
-            Console.WriteLine("1) 한산도대첩");
-            Console.WriteLine("2) ");
-            Console.WriteLine("3) ");
-            Console.WriteLine("4) ");
-            Console.WriteLine("====================================");
-            Console.Write("정답을 입력하세요 >>>  ");
-
-            // 유효성 검증
-            string answer7 = Console.ReadLine();
-            int iAnswer7;
-            bool isNum7 = int.TryParse(answer7, out iAnswer7);
-
-
-            if (isNum7)
-            {
-                if (iAnswer7 == 1)
-                {
-                    Console.WriteLine("정답입니다!");
-                    count++;
-                }
-                else
-                {
-                    Console.WriteLine("틀렸습니다.");
-                }
-
-            }
-
-            Console.WriteLine("질문 8. 자유당의 부정과 3.15 부정 선거를 계기로 일어난 혁명의 이름은?");
-            Console.WriteLine("1) ");
-            Console.WriteLine("2) 4.19혁명");
-            Console.WriteLine("3) ");
-            Console.WriteLine("4) ");
-            Console.WriteLine("====================================");
-            Console.Write("정답을 입력하세요 >>>  ");
-
-            // 유효성 검증
-            string answer8 = Console.ReadLine();
-            int iAnswer8;
-            bool isNum8 = int.TryParse(answer8, out iAnswer8);
-
-
-            if (isNum8)
-            {
-                if (iAnswer8 == 2)
-                {
-                    Console.WriteLine("정답입니다!");
-                    count++;
-                }
-                else
-                {
-                    Console.WriteLine("틀렸습니다.");
-                }
-
-            }
-
-            Console.WriteLine("질문 9. 임시 정부의 주석으로 한인 애국단을 조직하여 독립운동을 한 분은? ");
-            Console.WriteLine("1) ");
-            Console.WriteLine("2) ");
-            Console.WriteLine("3) ");
-            Console.WriteLine("4) 김구");
-            Console.WriteLine("====================================");
-            Console.Write("정답을 입력하세요 >>>  ");
-
-            // 유효성 검증
-            string answer9 = Console.ReadLine();
-            int iAnswer9;
-            bool isNum9 = int.TryParse(answer9, out iAnswer9);
-
-
-            if (isNum9)
-            {
-                if (iAnswer9 == 4)
-                {
-                    Console.WriteLine("정답입니다!");
-                    count++;
-                }
-                else
-                {
-                    Console.WriteLine("틀렸습니다.");
-                }
-
-            }
-
-            Console.WriteLine("질문 10. 민족대표 33인이 독립 선언서를 낭독하고 만세 삼창을 한 곳은? ");
-            Console.WriteLine("1) 탑골공원");
-            Console.WriteLine("2) ");
-            Console.WriteLine("3) ");
-            Console.WriteLine("4) ");
-            Console.WriteLine("====================================");
-            Console.Write("정답을 입력하세요 >>>  ");
-
-            // 유효성 검증
-            string answer10 = Console.ReadLine();
-            int iAnswer10;
-            bool isNum10 = int.TryParse(answer10, out iAnswer10);
-
-
-            if (isNum10)
-            {
-                if (iAnswer10 == 1)
-                {
-                    Console.WriteLine("정답입니다!");
-                    count++;
-                }
-                else
-                {
-                    Console.WriteLine("틀렸습니다.");
-                }
-
-            }
-            // 10문제가 출제 끝
-
-            float correctResult = ((float)count / 10)*100;
-
-            Console.WriteLine("총 10개의 문제 중에 " + count + "개의 문제를 맞추셨습니다. 정답률은 " + correctResult + "% 입니다.");
 
 
         }
