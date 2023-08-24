8253a6eaab82b1c7afa5d0d81a8dd948013277e4
Sujin Choi(최수진 - Q-Service Dev. Team) <98chltnwls@gmail.com>
2023-08-24 18:12:12 +0900

    .

--- WebFormEx/BMI/FileSearchResult.aspx
+++ WebFormEx/BMI/FileSearchResult.aspx
@@ -6,12 +6,17 @@
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
-            <h1>검색어를 포함하는 파일 검색</h1>
-            <p>
+            <h1 class="auto-style1">검색어를 포함하는 파일 검색</h1>
+            <p class="auto-style1">
                 <asp:HyperLink ID="lnkSearchString" Target="_blank" runat="server">HyperLink</asp:HyperLink>
             &nbsp;&nbsp;
                 <asp:Button ID="btnGoBack" runat="server" PostBackUrl="~/FileSearch.aspx" Text="이전페이지로 가기" />
---
+++ WebFormEx/BMI/FileSearchResult.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BMI
+{
+    public partial class FileSearchResult : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // 이전 페이지가 있을 경우
+            if (PreviousPage != null)
+            {
+                // 이전 페이지의 컨트롤 구하기
+                TextBox txtKeyWord;
+                txtKeyWord = (TextBox)PreviousPage.FindControl("txtKeyWord");
+
+                DropDownList ddlFileType;
+                ddlFileType = (DropDownList)PreviousPage.FindControl("ddlFileType");
+
+                // 이전 페이지의 입력 정보 처리
+                string url = "http://www.google.co.ke/search?q=";
+                url += Server.UrlEncode(txtKeyWord.Text + " ");
+
+                string fileType = "";
+
+                switch (ddlFileType.SelectedIndex)
+                {
+                    case 0:
+                        fileType = "filetype:doc";
+                        break;
+                    case 1:
+                        url += "filetype:ppt";
+                        break;
+                    case 2:
+                        fileType = "filetype:hwp";
+                        break;
+                    case 3:
+                        fileType = "filetype:pdf";
+                        break;
+                }
+                url += fileType;
+
+                // 링크 url 설정
+                lnkSearchString.NavigateUrl = url;
+                //페이지에 표시할 텍스트 설정
+                lnkSearchString.Text = txtKeyWord.Text + " " + fileType;
+            }
+
+        }
+    }
+}
